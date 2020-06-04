b4b0dd315cc53b694a312a0ab2f3bba4743e2857
Mudasirrr <hmudasir714@gmail.com>
2020-06-04 17:41:17 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <div className="App"><h1>Mudasir Hussain</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" /><br/><br/>
+        <h1>
+          Mudasir Hussain
+        </h1>
+      </header>
     </div>
   );
 }
